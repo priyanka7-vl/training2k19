a9000b4e6c2e9e804fc913b3221c85998d5c1910
priyanka7-vl <57670426+priyanka7-vl@users.noreply.github.com>
2019-12-30 11:01:19 +0530

    added js file

---
+++ exercise/js-exer.txt/20.js
@@ -0,0 +1,44 @@
+let input = [
+    {
+        id: 1, 
+        name: "priya", 
+        age: 23, 
+        hobbies:["dancing","cooking"]
+    }, 
+    {
+        id: 2, 
+        name: "kumar", 
+        age: 40, 
+        hobbies:["singing","cooking"]
+    },
+    {
+        id: 3, 
+        name: "virat", 
+        age: 37, 
+        hobbies:["reading"]
+    },
+    {
+        id: 4, 
+        name: "lucky", 
+        age: 62, 
+        hobbies:["painting","reading"]
+    },
+    {
+        id: 5, 
+        name: "kunal", 
+        age: 50, 
+        hobbies:["dancing"]
+    },
+];
+
+let hobbies = [];
+let result = {};
+input.forEach(person=>{
+    hobbies = [...hobbies,...person.hobbies];
+    hobbies = [...new Set(hobbies)];
+});
+for(let hobby of hobbies) {
+    let hobbienames = input.filter(val => val.hobbies.includes(hobby)).map((match_name => match_name.name));
+    result[hobby] = hobbienames;
+}
+console.log(result);
